a822a95c998cf55d9367330a93458c774a6ceeeb
charles <iviacharles@gmail.com>
2025-03-19 17:05:13 +0300

    raw sockets

--- websockets/exercise-raw/backend/server.js
+++ websockets/exercise-raw/backend/server.js
@@ -6,6 +6,7 @@ import nanobuffer from "nanobuffer";
 import objToResponse from "./obj-to-response.js";
 import generateAcceptValue from "./generate-accept-value.js";
 import parseMessage from "./parse-message.js";
+import { console } from "inspector";
 
 let connections = [];
 const msg = new nanobuffer(50);
@@ -24,11 +25,48 @@ const server = http.createServer((request, response) => {
   });
 });
 
-/*
- *
- * your code goes here
- *
- */
+server.on("upgrade", (request, socket) => {
+  if (request.headers["upgrade"] !== "websocket") {
+    socket.end("HTTP/1.1 400 Bad Request");
+    return;
+  }
+
+  const acceptKey = request.headers["sec-websocket-key"];
+  const acceptValue = generateAcceptValue(acceptKey);
+  const headers = [
+    "HTTP/1.1 101 Web Socket Protocol Handshake",
+    "Upgrade: websocket",
+    "Connection: Upgrade",
+    `Sec-WebSocket-Accept: ${acceptValue}`,
+    `Sec-WebSocket-Protocol: json`,
+  ];
+
+  socket.write(headers.join("\r\n") + "\r\n\r\n");
+  socket.write(objToResponse({ msg: getMsgs() }));
+  connections.push(socket);
+
+  socket.on("data", (buffer) => {
+    const message = parseMessage(buffer);
+
+    if (message) {
+      msg.push({
+        user: message.user,
+        text: message.text,
+        time: Date.now(),
+      });
+
+      connections.forEach((s) => {
+        s.write(objToResponse({ msg: getMsgs() }));
+      });
+    } else if (message === null) {
+      socket.end();
+    }
+  });
+
+  socket.on("end", () => {
+    connections = connections.filter((s) => s !== socket);
+  });
+});
 
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
